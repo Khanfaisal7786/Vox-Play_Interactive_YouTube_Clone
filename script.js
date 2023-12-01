cfdaa19558fab0dea22e0b3b2239971902072cfc
Faisal Khan <khanfaisal7786@gmail.com>
2023-12-02 02:29:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let menuIcon = document.querySelector(".menu-icon");
+let sideBar = document.querySelector(".sidebar");
+let container = document.querySelector(".container");
+
+
+menuIcon.onclick = function () {
+    sideBar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+}
